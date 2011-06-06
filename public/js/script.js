1631bb9f9bed954e48ca42af2369419007b9d4d4
Swizec Teller <swizec@swizec.com>
2011-06-06 02:19:09 +0200

    login is part of App

--- public/js/script.js
+++ public/js/script.js
@@ -6,26 +6,12 @@ $(function(){
     };
 
     now.logged_in = function () {
-        window.PopUp.close();
-
-        $.getJSON('/friends', {user: window.clientId}, function () {});
+        App.logged_in();
     };
 
     now.ready(function(){
         now.initiate(function (clientId) {
-            window.clientId = clientId;
-            console.log("I am client "+clientId);
-
-            var url = "/twitter_login?userid="+clientId;
-
-            $("#login-twitter").attr("href", url)
-                               .click(function (event) {
-                                   event.preventDefault();
-
-                                   window.PopUp = window.open(url,
-                                                              'Login',
-                                                              'width=600,height=400');
-                               });
+            App.enable_login(clientId);
         });
     });
 });
@@ -127,12 +113,35 @@ $(function () {
         el: $("#main"),
 
         initialize: function () {
+            _.bindAll(this, "enable_login", "logged_in");
+
             this.loader = new LoaderView;
 
             Friends.bind("add", function (friend) {
                 var view = new FriendView({model: friend});
                 App.el.append(view.render());
             });
+        },
+
+        enable_login: function (clientId) {
+            this.clientId = clientId;
+
+            var url = "/twitter_login?userid="+clientId;
+
+            $("#login-twitter").attr("href", url)
+                               .click(function (event) {
+                                   event.preventDefault();
+
+                                   App.PopUp = window.open(url,
+                                                           'Login',
+                                                           'width=600,height=400');
+                               });
+        },
+
+        logged_in: function () {
+            this.PopUp.close();
+
+            $.getJSON('/friends', {user: this.clientId}, function () {});
         }
     });
 
